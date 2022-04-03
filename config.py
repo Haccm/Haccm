7937fb987547c7331c45d34b16fa82a027e9459c
Haccm <89235740+Haccm@users.noreply.github.com>
2022-04-03 23:08:24 +0500

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+# Слито в https://t.me/HACKER_PHONE_VIP
+
+API_TOKEN = '5231157725:AAFPnXTC5vDDKs2DaQFDcDCZIc1OQ5W4FaI' # Токен бота! Брать с @BotFather
+admin = 1857769346 # ID Админа, тобишь твой! Брать с @getmyid_bot
+
+# Слито в https://t.me/HACKER_PHONE_VIP
